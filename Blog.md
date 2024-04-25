6489394eca5942aab7d01b0b91bd7a2642a5c7aa
abil-nur <abil.nurgaliyev@gmail.com>
2024-04-24 19:23:03 -0700

    Need a DB

--- Blog.md
+++ Blog.md
@@ -15,8 +15,9 @@ like to use MCMC (Markov-Chain-Monte-Carlo) to run simulations
 of the tournament with different team compositions.
 
 (a) The collection of data is quite straight forward. Games will 
-be played, and 
+be played, and I will collect: Player, Drink of choice, Game number, against whom played, hits made, saves made, cans drunk, turns taken to win.
 
-(b) For this I will use 
+(b) This part is handled by Ali. During the tournament our database
+will be multiple sheets of paper.
 
-(c)
+(c) I will be using R to interpret the data. Right now I have a couple of questions which I want answered: Who is the MVP, What is the ranking of players, What was the best team, What was the worst team, Graph Hit% and Liquor Drank, Save% and Liquor Drank.
--- data_analysis/play_random_tournament.py
+++ data_analysis/play_random_tournament.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import truncnorm
+from random import choice
 
 class Player:
     def __init__(self, num):
@@ -7,14 +8,27 @@ class Player:
         self.hit_pr = truncnorm.rvs(0.02,1)
         self.save_pr = truncnorm.rvs(0,1)
         self.ml_t = 355*self.hit_pr/truncnorm.rvs(1,10) #take around 5 hits to drink one can
+        self.doc = choice([0.0, 2.5, 4.0, 5.0, 5.5, 7.0])
     def __str__(self):
         return f"{self.num}: Hit_pr[{self.hit_pr}], Save_pr[{self.save_pr}], Ml_p_t[{self.ml_t}]"
     
-class Game:
-    def __init__(self, players1, players2):
-        self.home = players1
-        self.away = players2
+class Poule_Game:
+    def __init__(self, player1, player2):
+        self.home = player1
+        self.away = player2
+    def generate():
+        """
+        Generates the hits made, saves made, cans drunk, and turns to win
+        for both players
+        """
         
+        pass
+
+class Tournament_Game:
+    def __init__(self, team1, team2):
+        self.team1 = team1
+        self.team2 = team2
+
 def generate_players(n):
     """
     N amount of players are generated, each has a mean 
