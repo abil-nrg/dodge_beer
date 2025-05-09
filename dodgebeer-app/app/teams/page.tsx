7ffd4846ef9e9ac8342a762eba2db04e9f16b332
Ali Gaineshev <ggaineshev@gmail.com>
2025-05-09 00:14:08 -0700

    add react-loading-skeleton

--- dodgebeer-app/app/teams/page.tsx
+++ dodgebeer-app/app/teams/page.tsx
@@ -4,34 +4,73 @@ import TeamCard from "@/app/components/ui/TeamCard/TeamCard";
 import { ApiClient } from "@/app/api/all-routes";
 
 import styles from "@/app/teams/page.module.css";
-import { GetAllTeamsResponse } from "@/types/api";
+import { ApiResponse, ResponseWithErrorInData } from "@/types/api";
+import { GetAllTeamsResponse } from "@/types/team";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 export default function TeamsPage() {
   const [data, setData] = useState<GetAllTeamsResponse | null>(null);
   const [isError, setIsError] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function fetchTeams() {
-      const response = await ApiClient.GetAllTeams();
-      if (response.status !== 200) {
-        setIsError(true);
-        return;
-      }
+      // try to get response and set data
+      try {
+        const response = await ApiClient.GetAllTeams();
+        const result = (await response.json()) as ApiResponse<
+          GetAllTeamsResponse | ResponseWithErrorInData
+        >;
 
-      const data = (await response.json()) as GetAllTeamsResponse;
-      console.log(data);
-      setData(data || []);
+        if (response.status !== 200 || !result.success) {
+          console.error(
+            "API Error in fetchTeams():",
+            (result.data as ResponseWithErrorInData).error,
+          );
+          setIsError(true);
+          return;
+        }
+
+        setData(result.data as GetAllTeamsResponse);
+      } catch (err) {
+        // failed at some point
+        console.error("Fetch error:", err);
+        setIsError(true);
+      } finally {
+        // loading stop
+        setIsLoading(false);
+      }
     }
 
     fetchTeams();
   }, []);
 
-  // loading state
-  if (!data) {
-    return <div>Loading...</div>;
+  //-----------------------------------------------------------------------------//
+  if (isLoading) {
+    return (
+      <div className={styles["teams-container"]}>
+        {Array.from({ length: 3 }).map((_, idx) => (
+          <div key={idx} className={styles["skeleton-card"]}>
+            <Skeleton
+              height={450}
+              borderRadius={16}
+              containerClassName={"flex-1"}
+            />
+          </div>
+        ))}
+      </div>
+    );
   }
 
-  const hasTeams = Object.entries(data.teams).length > 0;
+  if (isError || !data) {
+    return (
+      <div className={styles["teams-container"]}>Failed to load teams.</div>
+    );
+  }
+
   const teamEntries = Object.entries(data.teams);
+  const hasTeams = teamEntries.length > 0;
 
   return (
     <div className={styles["teams-container"]}>
